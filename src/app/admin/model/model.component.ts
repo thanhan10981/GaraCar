cd7314fc8283d0328b1b7cc1a3a045ca34025d7b
thanhan10981 <152773355+thanhan10981@users.noreply.github.com>
2025-05-23 13:13:35 +0700

    Update model.component.ts

--- src/app/admin/model/model.component.ts
+++ src/app/admin/model/model.component.ts
@@ -25,4 +25,72 @@ export interface BaoCaoHoaDon {
   TongTien: number;
   TrangThai: string;
 }
+export interface KhachHang {
+  MaKhachHang: string;        
+  TenKhachHang: string;      
+  SoDienThoai?: string;         
+  NgaySinh?: string;          
+  GioiTinh?: string;    
+  DiaChi?: string;            
+  LoaiKhach: string; 
+  MaSoThue?: string;          
+  CmndCccd?: string;          
+  Email?: string;             
+  Facebook?: string;         
+  GhiChu?: string;           
+  HinhAnh?: string; 
+  TrangThai?:String;
+  NguoiTao?:String; 
+  NgayTao?:string;   
+  selected?: boolean;      
+}
+export interface NhaCungCap {
+  MaNhaCungCap: string;   
+  TenNhaCungCap: string;  
+  DienThoai: string;     
+  DiaChi: string;         
+  phuongXa: string;      
+  Email: string;          
+  CongTy: string;         
+  MaSoThue: string;       
+  NhomNCC: string;        
+  GhiChu: string; 
+  HinhAnh: string;          
+}
+export interface NhanVien {
+  MaNhanVien: string;      
+  TenNhanVien: string;     
+  DienThoai: string;       
+  NgaySinh: Date;          
+  GioiTinh: string;        
+  DiaChi: string;          
+  NgayBatDau: Date;        
+  ChucDanh: string;        
+  TaiKhoanDangNhap: string;
+  CmndCccd: string;        
+  Email: string;           
+  Facebook: string;        
+  GhiChu: string;          
+  HinhAnh: string;         
+}
+export interface SoQuy {
+  MaPhieu: string;         
+  ThoiGian: Date;          
+  GiaTri: number;          
+  NguoiNhan: string;       
+  SoDienThoai: string;     
+  DiaChi: string;          
+  LoaiThuChi: string;      
+  TrangThai: string;       
+  NguoiTao: string;        
+  NhanVien: string;        
+  DoiTuongNhan: string;   
+  GhiChu: string;          
+}
+
+
+
+
+
+
 
