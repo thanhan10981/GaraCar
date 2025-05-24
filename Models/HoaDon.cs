21cb0a73ffb3e20b12714aab30e02d3cefc5c4cd
truongngochan123 <144572122+truongngochan123@users.noreply.github.com>
2025-05-24 15:06:24 +0700

    Update HoaDon.cs

--- Models/HoaDon.cs
+++ Models/HoaDon.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+using GARA.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace GaraCarAPI.Models
 {
@@ -6,20 +7,20 @@ namespace GaraCarAPI.Models
     {
         [Key]
         public string MaHoaDon { get; set; }
-        public string MaSanPham { get; set; }
         public string MaKhachHang { get; set; }
-        public int SoLuong { get; set; }
         public DateTime ThoiGian { get; set; }
         public DateTime NgayGiaoDuKien { get; set; }
         public string NguoiBan { get; set; }
         public string TrangThai { get; set; }
-        public string? GhiChu { get; set; }
+        public string GhiChu { get; set; }
         public decimal PhuThu { get; set; }  
 
         public decimal TongTien { get; set; }
+        public string PhuongThucThanhToan { get; set; }
+        public ICollection<SanPhamHoaDon> SanPhamHoaDons { get; set; }
 
-        public SanPham? SanPham { get; set; }
-        public KhachHang? KhachHang { get; set; }
-        public ICollection<PhieuBaoHanh>? PhieuBaoHanhs { get; set; }
+        public KhachHang KhachHang { get; set; }
+        public ICollection<PhieuBaoHanh> PhieuBaoHanhs { get; set; }
     }
 }
+// thay đổi hóa đơn bỏ mã sản phẩm đi để tạo thêm bảng sản phẩm khác để liên kết ( đéo có hiểu thì kệ mẹ tụi m)
