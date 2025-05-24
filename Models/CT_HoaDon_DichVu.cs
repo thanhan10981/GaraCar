4912dcf184794f3b77a68ff55079fdffc2cc778a
truongngochan123 <144572122+truongngochan123@users.noreply.github.com>
2025-05-24 15:10:08 +0700

    Update CT_HoaDon_DichVu.cs

--- Models/CT_HoaDon_DichVu.cs
+++ Models/CT_HoaDon_DichVu.cs
@@ -1,7 +1,8 @@
-﻿namespace GaraCarAPI.Models
+namespace GaraCarAPI.Models
 {
     public class CT_HoaDon_DichVu
     {
+        public string MaCTDV { get; set; }
         public string MaHoaDon { get; set; }
         public string MaDichVu { get; set; }
         public decimal DonGia { get; set; }
