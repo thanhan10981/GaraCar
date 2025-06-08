6889d14daa365a269f425764e5e1455ce653a456
h.vinh <164975418+webbanve@users.noreply.github.com>
2025-06-08 17:19:53 +0700

    Update yeucausuachua.service.ts

--- src/app/admin/service/yeucausuachua.service.ts
+++ src/app/admin/service/yeucausuachua.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { YeuCauSuaChua } from '../model/model.component';
+import { YeuCauSuaChua, PaginationResponse } from '../model/model.component';
 
 @Injectable({
   providedIn: 'root'
@@ -24,7 +24,7 @@ export class YeuCauSuaChuaService {
     denNgay?: Date,
     page: number = 1,
     pageSize: number = 10
-  ): Observable<YeuCauSuaChua[]> {
+  ): Observable<PaginationResponse<YeuCauSuaChua>> {
     let params = new HttpParams()
       .set('page', page.toString())
       .set('pageSize', pageSize.toString());
@@ -34,7 +34,7 @@ export class YeuCauSuaChuaService {
     if (tuNgay) params = params.set('tuNgay', tuNgay.toISOString());
     if (denNgay) params = params.set('denNgay', denNgay.toISOString());
 
-    return this.http.get<YeuCauSuaChua[]>(`${this.apiUrl}/filter`, { params });
+    return this.http.get<PaginationResponse<YeuCauSuaChua>>(`${this.apiUrl}/filter`, { params });
   }
 
   // Get repair request by ID
@@ -50,12 +50,13 @@ export class YeuCauSuaChuaService {
   }
 
   // Update repair request
-  updateYeuCauSuaChua(id: string, yeuCau: YeuCauSuaChua): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, yeuCau);
+  updateYeuCauSuaChua(id: string, data: YeuCauSuaChua): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, data);
   }
 
   // Delete repair request
   deleteYeuCauSuaChua(id: string): Observable<any> {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
+  
 } 
