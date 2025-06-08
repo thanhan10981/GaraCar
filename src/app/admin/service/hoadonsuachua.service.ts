a3b8216a6625dbbbb753ecf51d5626c27e108c85
h.vinh <164975418+webbanve@users.noreply.github.com>
2025-06-08 17:18:48 +0700

    Update hoadonsuachua.service.ts

--- src/app/admin/service/hoadonsuachua.service.ts
+++ src/app/admin/service/hoadonsuachua.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { HoaDonSuaChua, HoaDonSuaChuaUpdate } from '../model/model.component';
-
+import { HoaDonSuaChua, HoaDonSuaChuaUpdate, PaginationResponse } from '../model/model.component';
 
 @Injectable({
   providedIn: 'root'
@@ -32,25 +31,24 @@ export class HoaDonSuaChuaService {
     return this.http.delete(`${this.apiUrl}/${id}`);
   }
 
-  filterHoaDonSuaChua(params: {
-    search?: string,
-    trangThai?: string,
-    tuNgay?: string,
-    denNgay?: string,
+  filterHoaDonSuaChua(filters: {
+    search?: string;
+    trangThai?: string;
+    tuNgay?: string;
+    denNgay?: string;
     page?: number;
     pageSize?: number;
-  }): Observable<HoaDonSuaChua[]> {
-    let queryParams = new HttpParams();
+  }): Observable<PaginationResponse<HoaDonSuaChua>> {
+    let params = new HttpParams();
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value !== undefined && value !== null && value !== '') {
+        params = params.set(key, value);
+      }
+    });
 
-    if (params.search) queryParams = queryParams.set('search', params.search);
-    if (params.trangThai) queryParams = queryParams.set('trangThai', params.trangThai);
-    if (params.tuNgay) queryParams = queryParams.set('tuNgay', params.tuNgay);
-    if (params.denNgay) queryParams = queryParams.set('denNgay', params.denNgay);
-    if (params.page) queryParams = queryParams.set('page', params.page.toString());
-    if (params.pageSize) queryParams = queryParams.set('pageSize', params.pageSize.toString());
-
-    return this.http.get<HoaDonSuaChua[]>(`${this.apiUrl}/filter`, { params: queryParams });
+    return this.http.get<PaginationResponse<HoaDonSuaChua>>(`${this.apiUrl}/filter`, { params });
   }
+
   exportExcel(filters: {
     search?: string;
     trangThai?: string;
@@ -59,7 +57,7 @@ export class HoaDonSuaChuaService {
   }): Observable<Blob> {
     let params = new HttpParams();
     Object.entries(filters).forEach(([key, value]) => {
-      if (value != null && value !== '') {
+      if (value !== undefined && value !== null && value !== '') {
         params = params.set(key, value);
       }
     });
